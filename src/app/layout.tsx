86905881dae90229c1cb219dcbcd1f5516808e27
AliHassan <124139541+AliHassan2712@users.noreply.github.com>
2026-04-08 20:43:55 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Cairo } from 'next/font/google'
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "../providers/Providers";
 import { Toaster } from "react-hot-toast"; 
